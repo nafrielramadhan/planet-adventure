29be051a8e4de90a6064584dc28583e99ea84047
nafrielramadhan <muhammad.nafriel@gmail.com>
2025-05-28 11:40:40 +0700

    done UAS

--- scripts/fall_indicator.gd
+++ scripts/fall_indicator.gd
@@ -8,15 +8,13 @@ var linked_enemy: Node = null
 func _process(delta):
 	if not player:
 		return
-	
-	# Hapus saat falling enemy hilang
+
 	if linked_enemy and not is_instance_valid(linked_enemy):
 		queue_free()
 		return
 
-	# Posisi sesuai dengan posisi falling enemy
 	if linked_enemy:
 		target_world_position = linked_enemy.global_position
 
-	var camera_offset_x = target_world_position.x - player.global_position.x
-	position = Vector2(camera_offset_x + get_viewport().size.x / 2, 40)
+	var screen_position: Vector2 = get_viewport().get_canvas_transform() * target_world_position
+	position = Vector2(screen_position.x, 40)
